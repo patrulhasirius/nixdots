9d4960c34f92015dd70397dcb91b958648cc05a3
Lucas Ribeiro <patruop@gmail.com>
2024-06-15 19:54:42 -0300

    change git credentials

--- home/lucas/tools/git/default.nix
+++ home/lucas/tools/git/default.nix
@@ -1,8 +1,8 @@
 _: {
   programs.git = {
     enable = true;
-    userName = "Redyf";
-    userEmail = "mateusalvespereira7@gmail.com";
+    userName = "patrulhasirius";
+    userEmail = "patruop@gmail.com";
     extraConfig = {
       init = {defaultBranch = "main";};
       core.editor = "nvim";
