ed3207451b3cac346b54e51d0e1eeedf26df6c0e
patrulhasirius <patruop@gmail.com>
2024-06-22 16:29:38 -0300

    add default python packages

--- home/lucas/tools/languages/python/default.nix
+++ home/lucas/tools/languages/python/default.nix
@@ -2,5 +2,12 @@
   home.packages = with pkgs; [
     poetry
     python3
+    python312Packages.numpy # these two are
+    python312Packages.scipy # probably redundant to pandas
+    python312Packages.jupyterlab
+    python312Packages.pandas
+    python312Packages.statsmodels
+    python312Packages.scikitlearn
+    python312Packages.polars
   ];
 }
